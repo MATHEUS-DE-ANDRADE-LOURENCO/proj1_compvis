3f27a1d559e20a99a4d65568a74b9c41092a9121
Lucas Tuon <136507356+LucasTuon@users.noreply.github.com>
2026-03-31 10:28:25 -0300

    Adicionando Corretamente os arquivos

--- src/utils.c
+++ src/utils.c
@@ -3,9 +3,11 @@
 * - Matheus de Andrade Lourenço (RA 10419691)
 * - Murillo Cardoso Ferreira (RA 10418082)
 * - Pietro Zanaga Neto (RA 10418574)
+* - Lucas Tuon de Matos (RA 10417987)
 */
 
 #include "utils.h"
+
 
 SDL_AppResult initialize_SDL_Windows_Renderers(Window *window1, Window *window2, Image *image)
 {
@@ -17,6 +19,12 @@ SDL_AppResult initialize_SDL_Windows_Renderers(Window *window1, Window *window2,
     {
         SDL_Log("\t-Erro ao iniciar a SDL: %s", SDL_GetError());
         SDL_Log("< Encerrando inicializacao");
+        return SDL_APP_FAILURE;
+    }
+
+    // Inicia a SDL_ttf
+    if (!TTF_Init()) {
+        SDL_Log("\t-Erro ao iniciar a SDL_ttf: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
@@ -182,11 +190,144 @@ void destroy_window(Window *window)
     }
 }
 
+// Classificacao da media da intensidade entre clara, media ou escura
+const char* calculate_brightness_level(float mean) {
+    if (mean < 85.0f) return "Escura";
+    if (mean < 170.0f) return "Media";
+    return "Clara";
+}
+
+// Classificacao do desvio padrao (contraste) entre alto, medio e baixo
+const char* calculate_contrast_level(float std_dev) {
+    if (std_dev < 40.0f) return "Baixo";
+    if (std_dev < 80.0f) return "Medio";
+    return "Alto";
+}
+
+void calculate_image_stats(Image *image, ImageStats *stats) {
+
+    if (!image || !image->surface || !stats) return;
+
+    // zerar histograma e variaveis
+    for (int i = 0; i <256; i++) stats->histogram[i] = 0;
+    double sum_intensities = 0;
+    int total_pixels = image->surface->w * image->surface->h;
+
+    // bloqueia a superficie pra nao ter alteracao nos pixels
+    SDL_LockSurface(image->surface);
+    Uint32 *pixels = (Uint32 *)image->surface->pixels;
+    const SDL_PixelFormatDetails *format = SDL_GetPixelFormatDetails(image->surface->format);
+
+    // contafem dos pixels
+    for (int i = 0; i < total_pixels; i++) {
+        Uint8 r, g, b, a;
+        SDL_GetRGBA(pixels[i], format, NULL, &r, &g, &b, &a);
+        
+        stats->histogram[r]++;
+        sum_intensities += r;
+
+    }
+    SDL_UnlockSurface(image->surface);
+
+    // calculo da media
+    stats->mean = (float)(sum_intensities / total_pixels);
+
+    // calculo do desvio padrao
+    double sum_sq_diff = 0;
+    for (int i = 0; i < 256; i++) {
+        double diff = i - stats->mean;
+        sum_sq_diff += stats->histogram[i] * (diff * diff);
+    }
+    stats->std_dev = sqrtf((float)(sum_sq_diff / total_pixels));
+
+    // atribuicao da intensidade e do contraste
+    stats->brightness_level = calculate_brightness_level(stats->mean);
+    stats->contrast_level = calculate_contrast_level(stats->std_dev);
+
+    SDL_Log("> Analise concluida: Media %.2f (%s), Desvio %.2f (%s)", 
+            stats->mean, stats->brightness_level, 
+            stats->std_dev, stats->contrast_level);
+}
+
+// Funcao auxiliar pra desenhar o texto
+void draw_text(SDL_Renderer *renderer, TTF_Font *font, const char *text, int x, int y) {
+    if (!font || !text) return;
+
+    SDL_Color white = {255, 255, 255, 255};
+    SDL_Surface *surface = TTF_RenderText_Blended(font, text, 0, white);
+    if (!surface) return;
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FRect dstRect = { (float)x, (float)y, (float)surface->w, (float)surface->h };
+
+    SDL_RenderTexture(renderer, texture, NULL, &dstRect);
+
+    SDL_DestroySurface(surface);
+    SDL_DestroyTexture(texture);
+}
+
+void render_histogram_graph(Window *win_interface, ImageStats *stats, TTF_Font *font) {
+    if (!win_interface || !win_interface->renderer || !stats) return;
+
+    // mudando o fundo da janela da interface
+    // essa funcao apaga o fundo por completo para evitar um histograma em cima do outro
+    // logo, seria bom usar camadas na hora de implementar o botão
+    SDL_SetRenderDrawColor(win_interface->renderer, 30, 30, 30, 255);
+    SDL_RenderClear(win_interface->renderer);
+
+    // Configurações de layout
+    int off_x = 80;
+    int off_y = 420;
+    int weight = 512;
+    int height = 250;
+
+    // Desenhar os eixos
+    SDL_SetRenderDrawColor(win_interface->renderer, 150, 150, 150, 255);
+    SDL_RenderLine(win_interface->renderer, (float)off_x, (float)off_y, (float)off_x, (float)(off_y - height)); // Eixo Y
+    SDL_RenderLine(win_interface->renderer, (float)off_x, (float)off_y, (float)(off_x + weight), (float)off_y); // Eixo X
+
+    // Procurar o valor maximo no histograma para normalizar a altura
+    int max_freq = 0;
+    for (int i = 0; i < 256; i++) {
+        if (stats->histogram[i] > max_freq) max_freq = stats->histogram[i];
+    }
+
+    // Desenhando o grafico
+    SDL_SetRenderDrawColor(win_interface->renderer, 200, 200, 200, 255); // Barras cinza claro
+    for (int i = 0; i < 256; i++) {
+        if (stats->histogram[i] > 0) {
+            float bar_h = ((float)stats->histogram[i] / max_freq) * height;
+            float x_pos = (float)(off_x + i * 2);
+            SDL_RenderLine(win_interface->renderer, x_pos, (float)off_y, x_pos, (float)(off_y - bar_h));
+        }
+    }
+
+    // Adicionar as classificaos e as labels
+    if (font) {
+        
+        draw_text(win_interface->renderer, font, "0", off_x - 5, off_y + 5);
+        draw_text(win_interface->renderer, font, "128", off_x + 256 - 15, off_y + 5);
+        draw_text(win_interface->renderer, font, "255", off_x + 512 - 15, off_y + 5);
+        draw_text(win_interface->renderer, font, "Intensidade", off_x + 200, off_y + 30);
+
+        // Informações da Análise
+        char buffer[128];
+        sprintf(buffer, "Media: %.2f (%s)", stats->mean, stats->brightness_level);
+        draw_text(win_interface->renderer, font, buffer, 50, 40);
+
+        sprintf(buffer, "Desvio Padrao: %.2f (%s)", stats->std_dev, stats->contrast_level);
+        draw_text(win_interface->renderer, font, buffer, 50, 70);
+    }
+
+
+    SDL_RenderPresent(win_interface->renderer);
+}
 
 
 void shutdown(void)
 {
     SDL_Log("> Encerrando o SDL");
+    TTF_Quit();
     SDL_Quit();
 }
 
